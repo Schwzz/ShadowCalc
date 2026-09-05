832b0c0fe48443cdd3c47ba92cb1ef724465972c
Swartzz <schwartzrdd@gmail.com>
2026-09-05 18:08:21 +0800

    Delete app directory

--- app/src/main/java/com/shadowcalc/app/AdBlocker.kt
+++
@@ -1,9 +0,0 @@
-package com.shadowcalc.app
-
-object AdBlocker {
-    private val adDomains = setOf("googleadservices.com","googlesyndication.com","google-analytics.com","doubleclick.net",
-        "adservice.google.com","googletagmanager.com","facebook.com/tr","connect.facebook.net","amazon-adsystem.com",
-        "ads.yahoo.com","advertising.com","adnxs.com","adsystem.com","moatads.com","outbrain.com","taboola.com",
-        "scorecardresearch.com","quantserve.com","googletagservices.com","googleads.g.doubleclick.net")
-    fun isAd(url: String): Boolean = adDomains.any { url.lowercase().contains(it) }
-}
