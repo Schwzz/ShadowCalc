6de0d07f290e5475e2b6250b0884ba7890b6b2da
Swartzz <schwartzrdd@gmail.com>
2026-09-05 20:37:55 +0800

    fix: add data binding compiler for view binding code generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+    kapt("com.android.databinding:compiler:8.2.0")
 }
