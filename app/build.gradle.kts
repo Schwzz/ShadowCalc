d597fd263c7b3adcc07589f49e0973a4479bb906
Swartzz <schwartzrdd@gmail.com>
2026-09-06 11:45:12 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,5 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.biometric:biometric:1.1.0")
-    implementation("com.google.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
